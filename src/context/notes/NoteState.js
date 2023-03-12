aab06cdc2cc96911b7e9602412fc82b6d84f1b60
Rajat Choudhary <rajatch133@gmail.com>
2023-03-12 15:54:49 +0530

    Removed code added to understand context in About

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,9 @@ import React , { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) =>{
-    const s1 = {
-        "name": "Rajat",
-        "age": "25"
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Rajat Choudhary",
-                "age": "26"
-            })
-        }, 1000);
-    }
+
     return (
-        <NoteContext.Provider value = {{state, update}}>
+        <NoteContext.Provider value = {{}}>
             {props.children}
         </NoteContext.Provider>
     )
